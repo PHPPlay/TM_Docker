2272dfc98bd7cd39eeff5aa0fdc06f3e903967c9
Dinis Cruz <dinis.cruz@owasp.org>
2015-06-03 18:59:46 +0100

    improved start-servers.sh command

--- start-servers.sh
+++ start-servers.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-echo ‘Starting TM_4_0_Design’ 
+echo Starting TM_4_0_Design
 
 cd TM_4_0_Design
-#npm install
-npm start &
-
-
-echo ‘Starting TM_4_0_GraphDB’ 
-
+node app.js &
 cd ..
-#npm install
+
+echo Starting TM_4_0_GraphDB
+
 cd TM_4_0_GraphDB
-npm run dev &
-
+node index.js &
 cd ..
-#npm install
 
 wait
