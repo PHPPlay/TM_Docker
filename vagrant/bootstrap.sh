d4f34ba3e48fa22dbd7552ec1c26a106bc37ed57
Dinis Cruz <dinis.cruz@owasp.org>
2015-06-05 19:54:27 +0100

    updated vagrant script

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,11 +1,21 @@
+# install dependencies
 apt-get update
+apt-get install -y linux-image-generic-lts-trusty wget git
 
-iapt-get install -y linux-image-generic-lts-trusty wget git
-
+# install docker
 wget -qO- https://get.docker.com/ | sh
-docker ps
 sudo usermod -aG docker vagrant
+docker ps
+
+ls
+pwd
+
+cd /home/vagrant
+
 
 ls
 
-
+# emabled bash colors
+echo "export CLICOLOR=1" >> .bashrc
+echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> .bashrc
+echo "export PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> .bashrc
---
+++ vagrant/tm-setup.sh
@@ -0,0 +1,14 @@
+# setup ssh keys
+cp /vagrant/keys/docker_key ./.ssh/id_rsa
+#sudo echo "IdentityFile ~/.ssh/id_rsa" >> /etc/ssh/ssh_config
+ssh-keyscan github.com >> ./.ssh/known_hosts
+
+# clone TM_Docker repo
+git clone git@github.com:TeamMentor/TM_Docker.git
+
+cd TM_Docker/inside-docker
+
+./build tm-node
+./build tm-design
+./build tm-qa
+./run rm-qa
