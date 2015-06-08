cdda409c613cd5e5f4f537f672d53e300d99f8e7
Dinis Cruz <dinis.cruz@owasp.org>
2015-06-08 08:31:41 +0100

    minor change

--- vagrant/tm-setup.sh
+++ vagrant/tm-setup.sh
@@ -1,7 +1,9 @@
 # setup ssh keys
 cp /vagrant/keys/docker_key ./.ssh/id_rsa
+cp /vagrant/keys/docker_key ./docker_key
 #sudo echo "IdentityFile ~/.ssh/id_rsa" >> /etc/ssh/ssh_config
 ssh-keyscan github.com >> ./.ssh/known_hosts
+
 
 # clone TM_Docker repo
 git clone git@github.com:TeamMentor/TM_Docker.git
@@ -11,4 +13,4 @@ cd TM_Docker/inside-docker
 ./build tm-node
 ./build tm-design
 ./build tm-qa
-./run rm-qa
+./run tm-qa
